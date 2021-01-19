c4e1a13ee4f770283d84b87a590119e826f09b3d
unknown <plinyosilvadev@gmail.com>
2021-01-19 17:59:47 -0300

    adicionado geolocation, permissão localização, vector-icons, botão localização atual

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
 import {View, Text, TouchableOpacity, StyleSheet, Platform, PermissionsAndroid, Alert } from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function Map() {
-    const [currentPosition, setPosition] = useState({
+    const [position, setPosition] = useState({
         latitude: 0.0000,
         longitude: 0.0000,
         latitudeDelta: 0.0020,
@@ -17,6 +19,63 @@ export default function Map() {
         longitudeDelta: 40.0100,
     });
 
+    const [watchID, setWatchID] = useState(0);
+
+    async function callLocation() {
+        if(Platform.OS === 'ios') {
+            await getLocation()
+            setInitialLocation(position)
+        }
+        else {
+            await requestLocationPermission(),
+            await getLocation(),
+            setInitialLocation(position)
+        }
+    };
+
+    async function requestLocationPermission(){
+        try {
+            const granted = await PermissionsAndroid.request(
+              PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+              {
+                title: 'Permissão de Localização',
+                message: 'Esta aplicação precisa da permissão de localização.',
+                buttonPositive: 'OK'
+              },
+            );
+            
+        } catch (err) {
+             Alert.alert('Permissão de localização negada');
+        } 
+    };
+
+    async function getLocation() {
+        Geolocation.getCurrentPosition(
+            pos => {
+                setPosition({
+                  ...position,
+                  latitude: pos.coords.latitude,
+                  longitude: pos.coords.longitude,
+                });
+            },
+            error => {
+                Alert.alert('Falha ao buscar sua localização. Verifique se o GPS está ativo.');
+              },
+            {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+        );
+        const watchID = Geolocation.watchPosition((position) => {
+            pos => {
+                setPosition({
+                  ...position,
+                  latitude: pos.coords.latitude,
+                  longitude: pos.coords.longitude,
+                });
+            }
+            
+        });
+        setWatchID(watchID)
+        
+    };
 
     return (
         <View style={styles.container}>
@@ -25,6 +84,14 @@ export default function Map() {
             region={initialLocation}
             showsUserLocation={true}
             />
+
+            <TouchableOpacity style={styles.locationButton} onPress={() => {callLocation()}}>
+                <Icon name="my-location" color={'#fff'} size={40} />
+            </TouchableOpacity>
+            <View style={styles.logo}>
+            <Text style={styles.logoText}>Map</Text>
+            <Text style={[styles.logoText, {color: '#e74c3c'}]}>Companys</Text>
+        </View>
           
         </View>
     );
@@ -38,5 +105,32 @@ const styles = StyleSheet.create({
       height: '100%',
       width: '100%',
     },
+    logo: {
+        backgroundColor: '#fff',
+        borderRadius: 15,
+        paddingHorizontal: 15,
+        elevation: 5,
+        marginTop: -730,
+        alignSelf: 'center',
+        marginRight: 10,
+        flexDirection: 'row',
+      },
+      logoText: {
+        fontWeight: 'bold',
+        fontSize: 22,
+      },
+      
+      locationButton: {
+        backgroundColor: '#e74c3c',
+        borderRadius: 150,
+        marginTop: -80,
+        width: 60,
+        height: 60,
+        alignSelf: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowColor: '#000',
+        elevation: 8,
+      },
     
   });
