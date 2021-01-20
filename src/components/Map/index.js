0b41518f26543aaa4e1a7cb7551dcbee7783a2dc
Plinyo Silva <plinyosilvadev@gmail.com>
2021-01-19 21:39:15 -0300

    alterações em react-native-maps

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -5,132 +5,117 @@ import Geolocation from '@react-native-community/geolocation';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function Map() {
-    const [position, setPosition] = useState({
-        latitude: 0.0000,
-        longitude: 0.0000,
-        latitudeDelta: 0.0020,
-        longitudeDelta: 0.0020,
-    });
+  const [position, setPosition] = useState({
+    latitude: -12.0000,
+    longitude: -54.0000,
+    latitudeDelta: 40.0100,
+    longitudeDelta: 40.0100,
+});
 
-    const [initialLocation, setInitialLocation] = useState({
-        latitude: -12.0000,
-        longitude: -54.0000,
-        latitudeDelta: 40.0100,
-        longitudeDelta: 40.0100,
-    });
+const [poi, setPoi] = useState({
+  latitude: 0,
+  longitude: 0,
+  });
 
-    const [watchID, setWatchID] = useState(0);
+  const request_location_runtime_permission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Permissão de Localização',
+          message: 'A aplicação precisa da permissão de localização.',
+          buttonPositive: 'OK'
+        },
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        getLocation()
+      } else {
+        Alert.alert('Permissão de localização negada');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    async function callLocation() {
-        if(Platform.OS === 'ios') {
-            await getLocation()
-            setInitialLocation(position)
-        }
-        else {
-            await requestLocationPermission(),
-            await getLocation(),
-            setInitialLocation(position)
-        }
-    };
-
-    async function requestLocationPermission(){
-        try {
-            const granted = await PermissionsAndroid.request(
-              PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-              {
-                title: 'Permissão de Localização',
-                message: 'Esta aplicação precisa da permissão de localização.',
-                buttonPositive: 'OK'
-              },
-            );
-            
-        } catch (err) {
-             Alert.alert('Permissão de localização negada');
-        } 
-    };
-
-    async function getLocation() {
-        Geolocation.getCurrentPosition(
-            pos => {
-                setPosition({
-                  ...position,
-                  latitude: pos.coords.latitude,
-                  longitude: pos.coords.longitude,
-                });
-            },
-            error => {
-                Alert.alert('Falha ao buscar sua localização. Verifique se o GPS está ativo.');
-              },
-            {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
-        );
-        const watchID = Geolocation.watchPosition((position) => {
-            pos => {
-                setPosition({
-                  ...position,
-                  latitude: pos.coords.latitude,
-                  longitude: pos.coords.longitude,
-                });
-            }
-            
+  async function getLocation(){
+    Geolocation.getCurrentPosition(
+      pos => {
+        setPosition({
+          ...position,
+          latitude: pos.coords.latitude,
+          longitude: pos.coords.longitude,
+          latitudeDelta: 0.0020,
+          longitudeDelta: 0.0020,
         });
-        setWatchID(watchID)
-        
-    };
-
-    return (
-        <View style={styles.container}>
-            <MapView
-            style={styles.map}
-            region={initialLocation}
-            showsUserLocation={true}
-            />
-
-            <TouchableOpacity style={styles.locationButton} onPress={() => {callLocation()}}>
-                <Icon name="my-location" color={'#fff'} size={40} />
-            </TouchableOpacity>
-            <View style={styles.logo}>
-            <Text style={styles.logoText}>Map</Text>
-            <Text style={[styles.logoText, {color: '#e74c3c'}]}>Companys</Text>
-        </View>
-          
-        </View>
+      },
+      error => {
+        console.log(error);
+        Alert.alert('Falha ao buscar loalização. Verifique o GPS.');
+      },
     );
+  }
+
+  async function onPressHandler(e) {
+    const coordinate = e.nativeEvent.coordinate
+  }
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        region={position}
+        showsUserLocation={true}
+        />
+
+      
+        <TouchableOpacity
+          style={styles.locationButton}
+          onPress={() => {
+            request_location_runtime_permission();
+          }}>
+          <Icon name="my-location" color={'#fff'} size={40} />
+        </TouchableOpacity>
+        <View style={styles.logo}>
+          <Text style={styles.logoText}>Locais no </Text>
+          <Text style={[styles.logoText, {color: '#e74c3c'}]}>Mapa</Text>
+        </View>
+    </View> 
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    map: {
-      height: '100%',
-      width: '100%',
-    },
-    logo: {
-        backgroundColor: '#fff',
-        borderRadius: 15,
-        paddingHorizontal: 15,
-        elevation: 5,
-        marginTop: -730,
-        alignSelf: 'center',
-        marginRight: 10,
-        flexDirection: 'row',
-      },
-      logoText: {
-        fontWeight: 'bold',
-        fontSize: 22,
-      },
-      
-      locationButton: {
-        backgroundColor: '#e74c3c',
-        borderRadius: 150,
-        marginTop: -80,
-        width: 60,
-        height: 60,
-        alignSelf: 'center',
-        justifyContent: 'center',
-        alignItems: 'center',
-        shadowColor: '#000',
-        elevation: 8,
-      },
-    
-  });
+  container: {
+    flex: 1,
+  },
+  map: {
+    height: '100%',
+    width: '100%',
+  },
+  logo: {
+    backgroundColor: '#fff',
+    borderRadius: 15,
+    paddingHorizontal: 15,
+    elevation: 5,
+    marginTop: -730,
+    alignSelf: 'center',
+    marginRight: 10,
+    flexDirection: 'row',
+  },
+  logoText: {
+    fontWeight: 'bold',
+    fontSize: 22,
+  },
+  
+  locationButton: {
+    backgroundColor: '#e74c3c',
+    borderRadius: 150,
+    marginTop: -100,
+    width: 70,
+    height: 70,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    elevation: 8,
+  },
+});
